882aed62d66cd53d11d22f76bacf57c63efbff8b
Adina Henderson <adinahenderson.mail@gmail.com>
2021-03-21 16:34:27 -0400

    Corrected js code - work in progress

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,15 +13,17 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 /* Propmt user for length of password and criteria for random password generation */
-function askUser() {
 var passwordLength = 0;
+function askUser() {
+  var passwordLength = prompt("Please enter the legnth of the desired password. It must be between 8 and 128 characters long");
   while (passwordLength < 8 || passwordLength > 128) {
-    passwordLength = prompt("Please enter length of desired password. It must be between 8 and 128 characters long");
+    passwordLength = prompt("Please enter a length between 8 and 128 characters");
   }
   userLowerCase = confirm ("Are you using lower case?");
   userUpperCase = confirm ("Should we add upper case?");
   userNumeric = confirm ("What about we include numeric characters?");
   userSpecialCharacters = confirm ("Any special characters?");
+  validateAnswers();
 }
 
  //Variables declared for each password criteria
@@ -40,15 +42,14 @@ function validateAnswers() {
         }
   else {
     askUser();
-    }
+  }
 }
-
-console.log(chars)
 
 // Function generatePassword select at random characters, multiplies by the length and adds/concatenates random characters
 function generatePassword(){
-  for (var i=0, i < passwordLength; i ++) {
+  askUser();
+  for (i = 0, i < passwordLength; i ++;) {
     var randomCharacters = Math.floor(Math.random()*chars.length);
     password += chars.substring(randomCharacters,randomCharacters+1);
   }
-}
+}}
