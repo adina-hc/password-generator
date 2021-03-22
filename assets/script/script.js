c0d8217d957f8b9e79249fe722769200f5d3d7c9
Adina Henderson <adinahenderson.mail@gmail.com>
2021-03-22 18:05:32 -0400

    Updated javaScript and README documents, correction of bugs and added alerts

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,5 @@
 
-// Assignment Code
+// Assign the HTML id of the Generate Password button
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -8,9 +8,10 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+/* Prompt user for password length and criteria for random password generation */
 
-/* Declase passwordLength variable and propmt user for length of password and criteria for random password generation */
-var passwordLength = 0;
+//var passwordLength = 0;
 
 function askUser(passwordLength) {
   while (passwordLength < 8 || passwordLength > 128) {
@@ -24,41 +25,44 @@ function askUser(passwordLength) {
 
  //Variables declared for each password criteria
 function validateAnswers() {
+  while (userLowerCase == false && userUpperCase == false && userNumeric == false && userSpecialCharacters == false) {
+    alert ("You must select at least one criteria to create a password")
+    askUser();
+  } 
   var chars = "";
     if (userLowerCase == true) {
-    chars += "qwertyuiopasdfghjklzxcvbnm";
-  }
-      if (userUpperCase == true) {
+      alert ("You chose to include lower case characters");
+      chars += "qwertyuiopasdfghjklzxcvbnm";
+    }
+    if (userUpperCase == true) {
+      alert ("You chose to include upper case characters");
       chars += "QWERTYUIOPASDFGHJKLZXCVBNM";
     }
-      if (userNumeric == true) {
-        chars += "1234567890";
-      }
-        if (userSpecialCharacters == true) {
-          chars += "~!@#$%^&*()_-=;',./|\+|}{:?><`"
-        }
-        if (userLowerCase == false && userUpperCase == false && userNumeric == false && userSpecialCharacters == false) {
-          alert ("You must select criteria to create a password")
-          askUser();
-        }
-        return chars;
+    if (userNumeric == true) {
+      alert ("You chose to include numeric characters");
+      chars += "1234567890";
+    }    
+    if (userSpecialCharacters == true) {
+    alert ("You chose to include special characters");
+      chars += "~!@#$%^&*()_-=;',./|\+|}{:?><`";
+    }
+    return chars;
 }
 
-// Function generatePassword select at random characters, multiplies by the length and adds/concatenates random characters
+// Function generatePassword select at random characters, multiplies by the length and concatenates random characters
 function generatePassword(){
   var password = "";
-  var passwordLength = window.prompt("Please enter the legnth of the desired password. It must be between 8 and 128 characters long");
+  var passwordLength = window.prompt("Please enter the length of the desired password. It must be between 8 and 128 characters long");
   askUser(passwordLength);
-  var chars = validateAnswers();
-  console.log(chars);
   console.log(passwordLength);
+  var chars = validateAnswers();
   for (i = 0; i < passwordLength; i++) {
     var randomCharacters = Math.floor(Math.random()*chars.length);
     password += chars.substring(randomCharacters,randomCharacters+1);
     }
   return password;
-}}
-
+  console.log(password);
+}
 
 // Add event listener to the generate button
 generateBtn.addEventListener("click", writePassword);
